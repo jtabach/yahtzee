491cd7cffa725311385fe8174df7d321d123f8d3
jtabach <jeff.n.tabachnick@gmail.com>
2016-01-01 21:57:24 -0800

    add dice images, dice layout, roller button

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+var dice = {
+    dice1: {
+        image: "Dice-1.png",
+        value: 1
+    },
+    dice1: {
+        image: "Dice-2.png",
+        value: 2
+    },
+    dice1: {
+        image: "Dice-3.png",
+        value: 3
+    },
+    dice1: {
+        image: "Dice-4.png",
+        value: 4
+    },
+    dice1: {
+        image: "Dice-5.png",
+        value: 5
+    },
+    dice1: {
+        image: "Dice-6.png",
+        value: 6
+    },
+}
